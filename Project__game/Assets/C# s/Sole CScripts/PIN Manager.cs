75a21f934d5b36534a3f1157acfc6780d5b7c84d
Saltyfish-123 <qq2083087214@outlook.com>
2026-04-07 17:28:09 +0800

    Scene4场景试验

---
+++ Project__game/Assets/C# s/Sole CScripts/PIN Manager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PINManager : MonoBehaviour
+{
+    struct NPC_Informs
+    {
+        public string name;
+    }
+
+}
