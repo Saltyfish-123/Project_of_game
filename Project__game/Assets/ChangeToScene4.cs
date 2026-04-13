dd3fb0078a0aabc2641a89c0b9520af879eb5706
Saltyfish-123 <qq2083087214@outlook.com>
2026-04-13 23:06:47 +0800

    游戏说明文档的胡乱生成

--- Project__game/Assets/ChangeToScene4.cs
+++ Project__game/Assets/ChangeToScene4.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class ChangeToScene4 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ChangeScene4()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
